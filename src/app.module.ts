1fc72dbb54352b5f6caa85e4c075b9aed04c173b
Guizorrr <guitofoli1806@gmail.com>
2026-04-18 21:24:49 -0300

    Instalado sequelize library, e integrado ao app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
